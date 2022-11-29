fd13449263c2f231fc99ccff79259ded7f81d3a9
devkbm <devkbm0417@gmail.com>
2022-11-29 16:48:23 +0900

    first commit

---
+++ src/main/java/com/like/ConnectBoot30Application.java
@@ -0,0 +1,13 @@
+package com.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectBoot30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectBoot30Application.class, args);
+	}
+
+}
