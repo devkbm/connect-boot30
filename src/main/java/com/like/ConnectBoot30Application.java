00ea42dd6f5564d10bcc2d4d104aa0e241829614
devkbm <devkbm0417@gmail.com>
2022-11-29 17:01:33 +0900

    최초 생성

--- src/main/java/com/like/ConnectBoot30Application.java
+++ src/main/java/com/like/ConnectBoot30Application.java
@@ -1,7 +1,16 @@
 package com.like;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @SpringBootApplication
 public class ConnectBoot30Application {
@@ -10,4 +19,16 @@ public class ConnectBoot30Application {
 		SpringApplication.run(ConnectBoot30Application.class, args);
 	}
 
+	@PersistenceContext
+	EntityManager em;
+
+	@Bean
+	public JPAQueryFactory queryFactory() {
+		return new JPAQueryFactory(em);
+	}
+	
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
